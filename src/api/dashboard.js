06a4f793d072d741ea25c0575fa4a8a5625c837a
Chris Dorward <chris.dorward@alcidion.com>
2016-09-20 16:30:02 +0930

    Add utilities folder

--- src/api/dashboard.js
+++
@@ -1,7 +0,0 @@
-import { getJson, urlBuilder } from './util';
-
-const URL_METRIC_DATA = '/dashboard.json';
-
-export async function getDashboardMetric() {
-    return await getJson(await urlBuilder(URL_METRIC_DATA));
-}
